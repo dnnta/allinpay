c57d2958ede95ff90b95ad51c9b0f6b2bc4cc7c5
ye.li <ye.li@mdslife.com>
2017-09-25 16:18:41 +0800

    change query

--- lib/allinpay/query.rb
+++ lib/allinpay/query.rb
@@ -1,5 +1,6 @@
 ###
-# 交易查询接口
+# 交易查询接
+# 1.支付交易结果查询
 #
 ###
 
@@ -8,10 +9,21 @@ module Allinpay
   module Query
     extend ActiveSupport::Concern
     included do
-      # 通联支付交易结果查询
-      def query_batch_pay(sn)
+
+      # 支付交易结果查询
+      #
+      # Paramters:
+      #
+      # sn: 交易序号
+      # options:
+      
+      def query_batch_pay(sn, options = {})
         params = set_infomation('200004')
-        tran_body = { MERCHANT_ID: merchant, QUERY_SN: sn, type: '1' }
+        tran_body = { MERCHANT_ID: merchant, QUERY_SN: sn }
+        rean_body[:TYPE] = options[:type] if options[:type]
+        rean_body[:STATUS] = options[:status] if options[:status]
+        rean_body[:START_DAY] = options[:start_day] if options[:start_day]
+        rean_body[:END_DAY] = options[:end_day] if options[:end_day]
         params[:QTRANSREQ] = tran_body
         res = conn.request(params)
         return result_wrap(:fail, res) if res["INFO"]["RET_CODE"] != "0000"
